f4fa419e0df7a4a864d76dac37321501835579f4
iturkan6 <iturkan6@gmail.com>
2022-07-13 12:00:24 +0400

    Materials for lesson 6

--- src/main/java/lesson6/Main.java
+++ src/main/java/lesson6/Main.java
@@ -2,11 +2,27 @@ package lesson6;
 
 public class Main {
 
+    enum Months{
+        MARCH,
+        APRIL
+    }
+
     public static void main(String[] args) {
-        for (DaysOfWeek i : DaysOfWeek.values()){
-            System.out.println(i);
+        System.out.println(DaysOfWeek.FRIDAY);
+//        for (DaysOfWeek i : DaysOfWeek.values()){
+//            System.out.println(i);
+//        }
+        int aa = aa(1);
+        int aa1 = aa(1, 5);
+        int aa2 = aa(new int[]{1, 2, 5});
+        for (int i = 0; i < 1000000; i++) {
+            Year year = new Year();
         }
 
+    }
+
+    public static int aa(int... arr){
+        return arr.length;
     }
 
 
