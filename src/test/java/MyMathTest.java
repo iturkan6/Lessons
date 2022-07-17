c145c4ef0cd3854f641fa609651a1096f72388bd
iturkan6 <iturkan6@gmail.com>
2022-07-17 12:44:27 +0400

    Materials for lesson 7

---
+++ src/test/java/MyMathTest.java
@@ -0,0 +1,25 @@
+import lesson7.MyMath;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyMathTest {
+
+    MyMath math;
+
+    @BeforeEach
+    public void setUp(){
+        this.math = new MyMath();
+    }
+
+    @Test
+    void sumTest(){
+        int result1 = math.sum(2, 1);
+        assertEquals(result1, 3);
+    }
+    @Test
+    void mulTest(){
+        int result1 = math.mul(3, 4);
+        assertEquals(result1, 12);
+    }
+}
