fe15e8d5341e9d8187cb4cb112640ad1f45a8edb
iturkan6 <iturkan6@gmail.com>
2022-07-29 11:43:05 +0400

    Materials for lesson 9

---
+++ src/main/java/lesson9/MyMath.java
@@ -0,0 +1,17 @@
+package lesson9;
+
+public class MyMath {
+    //Wite tests for this methods
+    public int sum(int a, int b){
+        return a + b;
+    }
+    public int mul(int a, int b){
+        return a * b;
+    }
+    public int sub(int a, int b){
+        return a - b;
+    }
+    public int div(int a, int b){
+        return a / b;
+    }
+}
