cf3d2aac59d0a994245aab4c34758e910c206fa4
iturkan6 <iturkan6@gmail.com>
2022-09-20 21:53:06 +0400

    Materials for lesson 5 (Month 2)

--- src/main/java/month2/lesson5/daoStructure/Main.java
+++ src/main/java/month2/lesson5/daoStructure/Main.java
@@ -3,7 +3,6 @@ package month2.lesson5.daoStructure;
 public class Main {
     public static void main(String[] args) {
         StudentDAO dao = new StudentDAO();
-        dao.addStudents();
         System.out.println(dao.getAll().toString());
     }
 }
