8ae3379af69af52cd6367595a09901f9b2878aeb
iturkan6 <iturkan6@gmail.com>
2022-10-30 14:18:00 +0400

    Add materials for month 3 lesson 10

--- les_3_10/src/main/java/com/example/demo/SpringApp.java
+++ les_3_10/src/main/java/com/example/demo/SpringApp.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KkjkjkjkjApplication {
-
+public class SpringApp {
 	public static void main(String[] args) {
-		SpringApplication.run(KkjkjkjkjApplication.class, args);
+		SpringApplication.run(SpringApp.class, args);
 	}
 
 }
---
+++ les_3_10/src/main/java/com/example/demo/controller/PersonController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Person;
+import com.example.demo.service.PersonService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+    private final PersonService service;
+
+    public PersonController(PersonService service) {
+        this.service = service;
+    }
+
+    @RequestMapping("/p1")
+    public Person handler1() {
+        return service.person1();
+    }
+
+    @RequestMapping("/p2")
+    public Person handler2() {
+        return service.person2();
+    }
+
+    @RequestMapping("/p3")
+    public Person handler3() {
+        return service.person3();
+    }
+}
