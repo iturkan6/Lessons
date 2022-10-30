d9c726d27b8eceb6a827520f22322cae3a2d60ee
iturkan6 <iturkan6@gmail.com>
2022-10-30 14:00:54 +0400

    d

---
+++ demo/src/main/java/com/example/demo/KkjkjkjkjApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkjkjkjkjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkjkjkjkjApplication.class, args);
+	}
+
+}
